ab9d59a695ee339000ac3df2db99971569d3ead2
SlobodanSkaric <slobodan.skaric@gmail.com>
2024-05-01 18:11:57 +0200

    Instal nessesary depenedensy

---
+++ config/dataBaseConfig.ts
@@ -0,0 +1,6 @@
+export const DatabaseConfiguration = {
+    "host" : "localhost",
+    "username" : "root",
+    "password": "",
+    "dbname": "docss"
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfiguration } from 'config/dataBaseConfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: DatabaseConfiguration.host,
+      port: 3306,
+      username: DatabaseConfiguration.username,
+      password: DatabaseConfiguration.password,
+      database: DatabaseConfiguration.dbname,
+      entities:[]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
